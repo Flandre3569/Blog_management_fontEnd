cf3de75d487755d49a7886307d5432cc12d71af6
Flandre3569 <1332726596@qq.com>
2021-12-20 00:32:19 +0800

    '其他页面数据响应'

--- src/views/blog.vue
+++ src/views/blog.vue
@@ -1,14 +1,89 @@
 <template>
-  <div>blog</div>
+  <div class="blog">
+    <el-row :gutter="20">
+      <el-col :span="24" v-for="blog in blogData" :key="blog.id"
+        ><el-card shadow="hover">
+          <el-descriptions title="BlogName">
+            <el-descriptions-item label="Id">{{
+              blog.id
+            }}</el-descriptions-item>
+            <el-descriptions-item label="author">{{
+              blog.author_id
+            }}</el-descriptions-item>
+
+            <el-descriptions-item label="Label">
+              <el-tag size="small">{{ blog.label_id }}</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="content">{{
+              blog.content
+            }}</el-descriptions-item>
+          </el-descriptions></el-card
+        ></el-col
+      >
+    </el-row>
+
+    <!-- 分页 -->
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :page-size="15"
+      :total="total"
+    >
+      >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
+import { ElNotification } from "element-plus";
+import mxRequest from "@/service/index";
 export default {
-  setup() {
-    return {};
+  created() {
+    const _this = this;
+    mxRequest
+      .get({
+        url: "blog/selectAll/1/5",
+      })
+      .then((res) => {
+        _this.blogData = res.data.content;
+        _this.total = res.data.totalElements;
+        _this.numberOfElements = res.data.totalElements;
+      });
+  },
+  data() {
+    return {
+      blogData: [],
+      numberOfElements: 0,
+    };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
--- src/views/label.vue
+++ src/views/label.vue
@@ -1,14 +1,106 @@
 <template>
-  <div>label</div>
+  <div class="label">
+    <el-tag
+      v-for="tag in dynamicTags"
+      :key="tag.id"
+      closable
+      :disable-transitions="true"
+      @close="handleClose(tag)"
+      size="medium"
+    >
+      {{ tag.name }}
+    </el-tag>
+    <el-input
+      v-if="inputVisible"
+      ref="saveTagInput"
+      v-model="inputValue.name"
+      class="input-new-tag"
+      size="medium"
+      @keyup.enter="handleInputConfirm"
+    >
+    </el-input>
+    <el-button v-else class="button-new-tag" size="medium" @click="showInput"
+      >+ New Tag</el-button
+    >
+  </div>
 </template>
 
 <script>
+import mxRequest from "@/service/index";
+import { reactive } from "vue";
 export default {
-  setup() {
-    return {};
+  created() {
+    const _this = this;
+    mxRequest
+      .get({
+        url: "label/selectAll",
+      })
+      .then((res) => {
+        _this.dynamicTags = res.data;
+      });
+  },
+  data() {
+    return {
+      dynamicTags: reactive([]),
+      inputVisible: false,
+      inputValue: reactive({
+        name: "",
+      }),
+    };
+  },
+  methods: {
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+      mxRequest.delete({
+        url: "/label/delete/" + tag.id,
+      });
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick((_) => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+
+    handleInputConfirm() {
+      const inputValue = this.inputValue;
+      mxRequest
+        .post({
+          url: "label/addLabel",
+          data: inputValue,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            this.dynamicTags.push(res.data.name);
+            this.$router.go(0);
+          }
+        });
+      this.inputVisible = false;
+      this.inputValue.name = "";
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang ="less" scoped>
+.el-tag + .el-tag {
+  /* margin-left: 100px; */
+  margin: 100px;
+}
+.button-new-tag {
+  margin-left: 100px;
+  height: 50px;
+  line-height: 50px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+}
+.el-tag {
+  font-size: 20px;
+}
 </style>
